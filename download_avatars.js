f9c0b1265dc631bd5a3296210a4a0fece5359821
Micah Britton <micahwkb@gmail.com>
2016-11-07 15:36:50 -0500

    started building pseudo and function layout for download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -2,16 +2,42 @@
 
 const request = require('request')
 const fs = require('fs')
+const https = require('https')
+const token = require('authData')
 
-let searchURL = "curl -u micahwkb:7c689bbe3b1887fc058079596ec3f9a35b8b7e5d https://api.github.com/users/"
+let searchData = {
+ url: "https://api.github.com/users/"
+ auth: {
+  user: "micahwkb",
+  pass:
+ }
+}
 
-// can append searchURL as follows:
-// searchURL += "testy"
-// console.log(searchURL)
+let userInput = process.argv.slice(2)
 
-userInput = process.argv.slice(2)
+
+// can append searchData as follows:
+// searchData += "testy"
+// console.log(searchData)
+
 console.log(userInput)
 
-userInput.forEach(username, function() {
+userInput.forEach(function(username) {
+  let avatarURL
+  searchData += username
+  request.get(searchData, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body)
+    } else {
+      console.log(error)
+    }
+  })
+         .on('response', function(response) {
+          console.log('Response Status Code: ', response.statusCode);
+          console.log('Response Message: ', response.statusMessage);
+          console.log('Content Type: ',response.headers['content-type'], '\n');
 
+         })
 })
+
+//variable for avatar.. data.avatar_url
