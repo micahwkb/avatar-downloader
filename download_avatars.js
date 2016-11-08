3a05ba0ed6a527621f7bddf2376226f81b07d3d0
Micah Britton <micahwkb@gmail.com>
2016-11-07 19:06:34 -0500

    updated console output, downloadImageByUrl function is fully functional

--- download_avatars.js
+++ download_avatars.js
@@ -20,84 +20,51 @@ const repoUrl = {
  }
 }
 
-const userURL = {
- url: 'https://api.github.com/',
- auth: {
-  user: authData.user,
-  pass: authData.token
- },
- headers: {
-  'User-Agent': 'Lighthouse Labs avatar exercise, via node.js'
- }
-}
-
-
-// if (owner == 'undefined' || repo == 'undefined') {
-//   console.log("Please provide the repo owner and project, in the form `node download_avatars.js foo bar`")
-// }
-
 let contribPath = owner + "/" + repo + "/contributors"
 
+function ifVariablesPassed() {
+  if (!owner || !repo) {
+    console.log("Please input a repository owner and project, in the form:")
+    console.log("example: `node download_avatars.js nodejs node`")
+  } else {
+    getRepoContributors(owner, repo, downloadImageByURL)
+  }
+}
+
+ifVariablesPassed()
+
 function getRepoContributors(owner, repo, cb) {
-  let contributorAvatarUrls = []
+  // let contributorAvatarUrls = []
   repoUrl.url += contribPath
   // console.log(repoUrl.url)
   request.get(repoUrl, function(error, response, body) {
     let repoData = JSON.parse(body)
     repoData.forEach(function(user) {
-      console.log("Retrieving avatar URl for " + user.login)
-      contributorAvatarUrls.push(user.avatar_url)
-    })
-    contributorAvatarUrls.forEach(function(url) {
-      cb(url)
+      let login = user.login
+      console.log("Retrieving avatar URl for " + login)
+      // contributorAvatarUrls.push(user.avatar_url)
+      let filePath = user.login + '.jpg'
+      cb(user.avatar_url, filePath)
     })
   })
 }
 
-getRepoContributors(owner, repo)
 
-const urlArray = []
-
-// function getAvatarUrl (uname) {
-//   userURL.url += "users/" + uname
-//   request.get(userURL, function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//       let avatarUrl = JSON.parse(body).avatar_url
-//       console.log("Getting avatar from:", avatarUrl)
-//       urlArray.push(avatarUrl)
-//       console.log(urlArray)
-//     } else {
-//       console.log(error)
-//     }
-//   })
-//          .on('response', function(response) {
-//             if(response.statusCode == 200) {
-//             }
-//          })
-// }
-
-// getAvatarUrl("telitos");
-//
-// userInput.forEach(function(username) {
-// })
-
-
-// works - outputs to stdout a given user's avatar_url
-
-// userInput.forEach(function(username) {
-//   searchData.url = searchData.url + username
-//   request.get(searchData, function (error, response, body) {
-//     if (!error && response.statusCode == 200) {
-//       avatarUrl = JSON.parse(body).avatar_url
-//       console.log(avatarUrl)
-//     } else {
-//       console.log(error)
-//     }
-//   })
-//          .on('response', function(response) {
-//             if(response.statusCode == 200) console.log('working...')
-
-//          })
-// })
-
-//variable for avatar.. data.avatar_url
+function downloadImageByURL(url, filePath) {
+  let dotty = "."
+  request.get(url, function(err, response, data){
+         if(err){
+           throw err
+         } else if(response.statusCode === 200) {
+              console.log('\nDownloaded ' + filePath)
+         }
+       })
+        .on('response', function(response) {
+          if (response.statusCode === 200) {
+            response.on('data', function(data) {
+            })
+          }
+        })
+        .pipe(fs.createWriteStream('./avatars/' + filePath))
+        console.log('test')
+}
