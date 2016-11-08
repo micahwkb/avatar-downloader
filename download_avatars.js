555b93d68bbb18d9484d98724a41ad791867c9ff
Micah Britton <micahwkb@gmail.com>
2016-11-07 20:20:03 -0500

    cleaned up slightly

--- download_avatars.js
+++ download_avatars.js
@@ -5,7 +5,6 @@ require('dotenv').config()
 const request = require('request')
 const fs = require('fs')
 const https = require('https')
-// similar to .env but laid out per Rafa's suggestion
 const userInput = process.argv.slice(2)
 const owner = userInput[0]
 const repo = userInput[1]
@@ -20,6 +19,34 @@ const repoUrl = {
  }
 }
 
+// download a given image path into /avatars directory
+function downloadImageByURL(url, filePath) {
+  let dotty = "."
+  request.get(url, function(err, response, data){
+         if(err){
+           throw err
+         }
+       })
+         .pipe(fs.createWriteStream('./avatars/' + filePath))
+        console.log('Saved ' + filePath + ' to ./avatars')
+      }
+
+
+// for a given repo, gather each contributor's avatar URL
+function getRepoContributors(owner, repo, cb) {
+  console.log("\nFetching contributor metadata...")
+  repoUrl.url += owner + "/" + repo + "/contributors"
+  request.get(repoUrl, function(error, response, body) {
+    let repoData = JSON.parse(body)
+    repoData.forEach(function(user) {
+      let login = user.login
+      console.log("\nRetrieving avatar for " + login)
+      let filePath = login + '.jpg'
+      // send to callback
+      cb(user.avatar_url, filePath)
+    })
+  })
+}
 
 // check for complete input at command-line
 function ifVariablesPassed() {
@@ -33,31 +60,3 @@ function ifVariablesPassed() {
 
 // call conditional immediately
 ifVariablesPassed()
-
-// for a given repo, gather each contributor's avatar URL
-function getRepoContributors(owner, repo, cb) {
-  console.log("\nFetching")
-  repoUrl.url += owner + "/" + repo + "/contributors"
-  request.get(repoUrl, function(error, response, body) {
-    let repoData = JSON.parse(body)
-    repoData.forEach(function(user) {
-      let login = user.login
-      console.log("\nRetrieving avatar URl for " + login)
-      let filePath = login + '.jpg'
-      // send to callback
-      cb(user.avatar_url, filePath)
-    })
-  })
-}
-
-// download a given image path into /avatars directory
-function downloadImageByURL(url, filePath) {
-  let dotty = "."
-  request.get(url, function(err, response, data){
-         if(err){
-           throw err
-         }
-       })
-        .pipe(fs.createWriteStream('./avatars/' + filePath))
-        console.log('Saved ' + filePath + ' to "avatars" folder')
-      }
